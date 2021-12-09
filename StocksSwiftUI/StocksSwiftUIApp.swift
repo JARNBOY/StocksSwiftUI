f3af8d9b51256a2d8b7db29e78d398b9bb88ed23
Papon <tattoo197@outlook.com>
2021-12-09 10:06:30 +0700

    Initial Commit

---
+++ StocksSwiftUI/StocksSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksSwiftUIApp.swift
+//  StocksSwiftUI
+//
+//  Created by Papon Supamongkonchai on 9/12/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct StocksSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
